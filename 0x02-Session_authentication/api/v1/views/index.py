27fbd5c272b7d4edd1e3dc1120c4cc3d5a3ee92f
Isaac Imonivwerha <isaacimonivwerha@gmail.com>
2024-11-16 21:55:59 +0000

    all tasks done

---
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+""" Documentation for the index
+"""
+from flask import Blueprint
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
+from api.v1.views.users import *
+from api.v1.views.session_auth import *
+
+User.load_from_file()
